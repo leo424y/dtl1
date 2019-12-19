8038fa1d0ccd0c6701a94196d37cff876b070865
Fly Chang <leo424y@gmail.com>
2019-12-19 10:35:29 +0800

    post title for searching

--- db/migrate/20191218103127_create_posts.rb
+++ db/migrate/20191218103127_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts, id: :uuid do |t|
       t.string :url
+      t.string :title
       t.jsonb :archive
       t.uuid :node_id
 
