98111c270ca39eef4a9d9a77c7f9e8ab40de2e02
Fly Chang <leo424y@gmail.com>
2019-12-20 15:31:46 +0800

    Node find_or_create_by Name - Facebook ID

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,8 @@ class Post < ApplicationRecord
         require 'csv'
         CSV.foreach(file.path, headers: true) do |row|
             row_hash = row.to_hash
-            Node.last.posts.create!(archive: row_hash, url: row.to_hash['URL'], title: row_hash['Message'])
+            node = Node.find_or_create_by(name: row_hash['Facebook Id'], source: 'facebook', url: "https://www.facebook.com/#{row_hash['Facebook Id']}" )
+            node.posts.create!(archive: row_hash, url: row_hash['URL'], title: row_hash['Message'])
         end
     end
 end
