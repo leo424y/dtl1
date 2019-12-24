6ba97ef73e654cd13cc976874e708d9722c1761e
Fly Chang <leo424y@gmail.com>
2019-12-24 10:53:38 +0800

    better links view

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
     def index
-        @links = Link.search(params[:url])
+        @links = Link.search(params[:url]).order(created_at: :desc).limit(100)
         respond_to do |format|
             format.html # index.html.erb
             format.xml  { render xml: @links }
