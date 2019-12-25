e483d8e58af6e7d55271fb9223e10e716cdccccf
Fly Chang <leo424y@gmail.com>
2019-12-25 14:38:57 +0800

    RF top links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,10 +1,7 @@
 class LinksController < ApplicationController
     def index
-        @link_tops = Link.group(:url).count.sort {|a,b| b[1] <=> a[1]}.select { |n| n[1]>2 }
-        @link_domains = Link.group(:url).count.sort {|a,b| b[1] <=> a[1]}.select { |n| n[1]>2 }.select { |n| n[0] = n[0] && n[0].split('/')[2] }.group_by { |a, b| a }.map{ |a, xs|
-            [a,
-             xs.count]   # true
-          }.sort {|a,b| b[1] <=> a[1]}
+        @link_tops = Link.top_group
+        @link_domains = Link.top_domain
         @links = Link.search(params[:url]).includes(:post).order(created_at: :desc).limit(100)
         respond_to do |format|
             format.html # index.html.erb
--- app/models/link.rb
+++ app/models/link.rb
@@ -8,4 +8,13 @@ class Link < ApplicationRecord
           limit(50)
         end
     end
-end
+
+    def self.top_group
+      group(:url).count.sort {|a,b| b[1] <=> a[1]}.select { |n| n[1]> 5 } 
+
+    end
+
+    def self.top_domain
+      top_group.select { |n| n[0] = n[0] && n[0].split('/')[2] }.group_by { |a, b| a }.map{ |a, xs| [a, xs.count]}.sort {|a,b| b[1] <=> a[1]}.select { |n| n[1]> 2 } 
+    end
+  end
