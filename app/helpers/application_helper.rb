bf0285022af5a6f844249d6dc7673872c651180b
Fly Chang <leo424y@gmail.com>
2019-12-26 17:38:20 +0800

    timezone ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
     def tw_time(x)
-        x.to_time ? (x.to_time + 8.hours).to_datetime : x
+        # x.to_time ? (x.to_time + 8.hours).to_datetime : x
+        x
     end
 end
