7d024b0a2a9147f0702f9fb02a9cd4fe64d4734a
Fly Chang <leo424y@gmail.com>
2019-12-26 14:04:23 +0800

    no limit

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,7 +2,7 @@ class LinksController < ApplicationController
     def index
         @link_tops = Link.top_group
         @link_domains = Link.top_domain
-        @links = Link.search(params[:url]).includes(:post).order(created_at: :desc).limit(100)
+        @links = Link.search(params[:url]).includes(:post).order(created_at: :desc)
         respond_to do |format|
             format.html # index.html.erb
             format.xml  { render xml: @links }
