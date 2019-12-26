5c355a6262b98c7ede0a63154f06181a8b065e2b
Fly Chang <leo424y@gmail.com>
2019-12-26 15:21:50 +0800

    .

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,15 @@
 class LinksController < ApplicationController
     def index
         @links = Link.search(params[:url])
-        @link_tops = @links.top_group
-        @link_domains = @links.top_domain
-        @links = @links.includes(:post).order(created_at: :desc)
+        if params[:url].present?
+            @link_tops = @links.top_group
+            @link_domains = @links.top_domain
+        else 
+            @link_tops = Link.top_group
+            @link_domains = Link.top_domain
+        end
+        @links = @links.includes(:post).order(created_at: :desc)    
+
         respond_to do |format|
             format.html # index.html.erb
             format.xml  { render xml: @links }
