e91736ea4d263f62514b696a4d505399039094b1
Fly Chang <leo424y@gmail.com>
2020-01-08 10:35:53 +0800

    search json

--- app/models/link.rb
+++ app/models/link.rb
@@ -13,18 +13,17 @@ class Link < ApplicationRecord
 
     def self.search_context(description)
       if description.present?
-        # TODO
-        none
+        where("archive ->> 'link_description' LIKE :language", language: "%#{description}%" )
       else
         none
       end
     end
 
-    def self.top_group
-      group(:url).count.sort {|a,b| b[1] <=> a[1]}.select { |n| (n[1]> 7)&&(n[0] != nil) } 
-    end
+    # def self.top_group
+    #   group(:url).count.sort {|a,b| b[1] <=> a[1]}.select { |n| (n[1]> 7)&&(n[0] != nil) } 
+    # end
 
-    def self.top_domain
-      top_group.select { |n| n[0] = n[0] && n[0].split('/')[2] }.group_by { |a, b| a }.map{ |a, xs| [a, xs.count]}.sort {|a,b| b[1] <=> a[1]}.select { |n| n[1]> 2 } 
-    end
+    # def self.top_domain
+    #   top_group.select { |n| n[0] = n[0] && n[0].split('/')[2] }.group_by { |a, b| a }.map{ |a, xs| [a, xs.count]}.sort {|a,b| b[1] <=> a[1]}.select { |n| n[1]> 2 } 
+    # end
   end
