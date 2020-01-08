20808353b290bead87c1c16538561b01f41d9fa7
Fly Chang <leo424y@gmail.com>
2020-01-08 16:26:49 +0800

    bug

---
+++ app/controllers/farms_controller.rb
@@ -0,0 +1,14 @@
+class FarmsController < ApplicationController
+    def index
+        require 'net/https'
+        token = ENV['CT_TOCKEN']
+        uri = URI("https://api.crowdtangle.com/links?link=#{params[:link]}&token=#{token}&sortBy=total_interactions&count=100&startDate=#{params[:start_date]}&endDate=#{params[:end_date]}")
+        request = Net::HTTP.get_response(uri)
+        rows_hash = JSON.parse(request.body) if request.is_a?(Net::HTTPSuccess)
+        
+        respond_to do |format|
+            format.json { render json: rows_hash }
+        end
+    end
+end
+
---
+++ app/models/farm.rb
@@ -0,0 +1,6 @@
+class Farm < ActiveRecord::Base
+  establish_connection(:farm)
+  self.table_name = "farms"
+end
+
+
