5395e9e7b9969ff7e1c59e1fffc4f29530b81139
Fly Chang <leo424y@gmail.com>
2020-01-08 14:09:21 +0800

    date

--- app/controllers/ctlinks_controller.rb
+++ app/controllers/ctlinks_controller.rb
@@ -2,7 +2,7 @@ class CtlinksController < ApplicationController
     def index
         require 'net/https'
         token = ENV['CT_TOCKEN']
-        uri = URI("https://api.crowdtangle.com/links?link=#{params[:link]}&token=#{token}&sortBy=total_interactions&count=100")
+        uri = URI("https://api.crowdtangle.com/links?link=#{params[:link]}&token=#{token}&sortBy=total_interactions&count=100&startDate=#{params[:start_date]}&endDate=#{params[:end_date]}")
         request = Net::HTTP.get_response(uri)
         rows_hash = JSON.parse(request.body) if request.is_a?(Net::HTTPSuccess)
         
