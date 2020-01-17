7060c0fe97579ab3e68fd4fc761ec6ce84125b3e
Fly Chang <leo424y@gmail.com>
2020-01-17 11:58:46 +0800

    lower update rate

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -10,10 +10,10 @@ s = Rufus::Scheduler.singleton
 
 # Stupid recurrent task...
 #
-s.every '10m' do
-  Post.ct_api_import
-end
+# s.every '10m' do
+# end
 
 s.every '30m' do
+  Post.ct_api_import
   Post.news_api_import
 end
