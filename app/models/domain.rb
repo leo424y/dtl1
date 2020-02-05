6b262820a862a87958b6da220effcfc089495641
Fly Chang <leo424y@gmail.com>
2020-02-05 15:10:15 +0800

    .

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,10 +1,9 @@
 class Domain < ApplicationRecord
     has_many :nodes
-    scope :order_by_nodes_count, lambda {
+    scope :order_by_nodes_count, -> {
         select("domains.*, count(nodes.id) AS nodes_count").
         joins(:nodes).                                                   
         group("domains.id").
         order("nodes_count DESC")
       }
-
 end
