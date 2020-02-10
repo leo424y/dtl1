c45edba44b2bf8033c4e35353262aefbcead1fc9
Fly Chang <leo424y@gmail.com>
2020-02-10 10:44:04 +0800

    domain counts

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -4,11 +4,14 @@ class DomainsController < ApplicationController
           @domains = Domain.search_date(params, 'created_at').order_by_nodes_count
         else
           @domains = Domain.order_by_nodes_count
-        end        
+        end
+
+        @domains_count = Domain.count
+
         respond_to do |format|
           format.html # index.html.erb
           format.xml  { render xml: @domains }
-          format.json { render json: @domains }
+          format.json { render json: {head: {count: @domains_count}, body: @domains } }
           format.csv { send_data @domains.to_csv("id url archive created_at updated_at"), filename: "domains-#{Date.today}-params-#{params.inspect}.csv" }
         end
     end
