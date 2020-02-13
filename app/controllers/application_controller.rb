12c64cdacdaacc5bc5364067426df9e1b5c7d819
Fly Chang <leo424y@gmail.com>
2020-02-13 16:02:42 +0800

    ENV IP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
     before_action :protect
 
     def protect
-      @ips = ['::1', '111.235.245.215'] #And so on ...]
-      if @ips.include? request.remote_ip
+      @ips = ENV['IPOK'].split(',')
+      unless @ips.include? request.remote_ip
          render action: "unauth"
       end
     end
