14c00aa249a210dd4533f9c10c78e94e3e67dc13
Fly Chang <leo424y@gmail.com>
2020-02-13 16:00:35 +0800

    IP block

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    before_action :protect
+
+    def protect
+      @ips = ['::1', '111.235.245.215'] #And so on ...]
+      if @ips.include? request.remote_ip
+         render action: "unauth"
+      end
+    end
+    
+    def unauth
+        render "pages/unauth"
+    end
 end
