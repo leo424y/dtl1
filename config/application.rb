d17b0984761caa6d9dddcafbbfe71db01833c9ee
Fly Chang <leo424y@gmail.com>
2020-02-21 12:25:17 +0800

    F: create time patch

--- config/application.rb
+++ config/application.rb
@@ -84,10 +84,58 @@ module Rumors
             end
             find_articles
           end
-
         end        
-
       end
+
+      module Utils
+        class ListArticles
+          def initialize(text)
+            @text = text
+          end
+
+          def purify_gql_query
+            gql_query.strip
+          end
+
+          def variables
+            { text: @text.to_s }
+          end
+
+          private
+
+          def gql_query
+            <<~GQL
+            query($text: String) {
+              ListArticles(
+                filter: { moreLikeThis: { like: $text } }
+                orderBy: [{ _score: DESC }]
+                first: 4
+              ) {
+                edges {
+                  node {
+                    id
+                    text
+                    createdAt
+                    updatedAt
+                    hyperlinks {
+                      url
+                    }
+                    articleReplies {
+                      reply {
+                        id
+                        text
+                        type
+                        reference
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            GQL
+          end
+        end
+      end      
     end
   end
 end
