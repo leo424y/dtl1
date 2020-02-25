f16b16339cb296245548e1d88c218f273e392419
Fly Chang <leo424y@gmail.com>
2020-02-25 15:20:19 +0800

    .

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -4,9 +4,13 @@ class Gene < ApplicationRecord
 
 
     def self.import(endpoints, params)
-        uri = URI("#{ENV['GENEAPI']}/#{endpoints}?date=#{params[:date]}")
+        begin
+        uri = URI("#{ENV['NEWS_API_ENDPOINT']}/#{endpoints}?date=#{params[:date]}")
         request = Net::HTTP.get_response(uri)
         rows_hash = JSON.parse(request.body)['news_rank'] if request.is_a?(Net::HTTPSuccess)
         rows_hash.merge params
+        rescue 
+            {}
+        end
     end
 end
