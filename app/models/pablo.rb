3eafbabce8f4bd5e302650507efaeacf3ddf99ac
Fly Chang <leo424y@gmail.com>
2020-04-08 11:10:51 +0800

    pablo timeout 28

--- app/models/pablo.rb
+++ app/models/pablo.rb
@@ -3,7 +3,7 @@ class Pablo < ApplicationRecord
         require 'net/http'
         uri = URI.parse("#{ENV['PABLO_API_KEYWORD']}&keyword=#{URI.escape(params[:description])}&position=1&emotion=1&startTime=#{params[:start_date]}&endTime=#{params[:end_date]}&pageIndex=1&pageRows=9999")
         begin
-            response = Timeout::timeout(20) { Net::HTTP.get_response(uri)}
+            response = Timeout::timeout(28) { Net::HTTP.get_response(uri)}
             rows_hash = JSON.parse(response.body)['body']['list'].select{|k| k['pubTime'].split(' ')[0].to_date.between?(params[:start_date].to_date, params[:end_date].to_date)}
             {pablo: {count: rows_hash.count, result: rows_hash}}
         rescue
