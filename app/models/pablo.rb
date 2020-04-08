719e42461e0f36cdd118113f6dcd44575c048a4a
Fly Chang <leo424y@gmail.com>
2020-04-08 11:53:16 +0800

    kiss

--- app/models/pablo.rb
+++ app/models/pablo.rb
@@ -1,13 +1,13 @@
 class Pablo < ApplicationRecord
     def self.result (params)
         require 'net/http'
-        uri = URI.parse("#{ENV['PABLO_API_KEYWORD']}&keyword=#{URI.escape(params[:description])}&position=1&emotion=1&startTime=#{params[:start_date]}&endTime=#{params[:end_date]}&pageIndex=1&pageRows=9999")
+        uri = URI.parse("#{ENV['PABLO_API_KEYWORD']}&keyword=#{URI.escape(params[:description])}&position=1&emotion=1&startTime=#{params[:start_date]}&endTime=#{params[:end_date]}&pageIndex=1&pageRows=99999")
         begin
             response = Timeout::timeout(28) { Net::HTTP.get_response(uri)}
             rows_hash = JSON.parse(response.body)['body']['list'].select{|k| k['pubTime'].split(' ')[0].to_date.between?(params[:start_date].to_date, params[:end_date].to_date)}
-            {pablo: {count: rows_hash.count, result: rows_hash}}
+            {result: {source: 'pablo', count: rows_hash.count, posts: rows_hash}}
         rescue
-            {pablo: {result: 'timeout'}}
+            {result: 'timeout'}
         end
     end
 end
