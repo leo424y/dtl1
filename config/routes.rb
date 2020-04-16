565440cb50d363c747a57a6b8ae35bfaa4b0750d
Fly Chang <leo424y@gmail.com>
2020-04-16 15:22:50 +0800

    lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,30 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/fin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :farms
   resources :pablos
-  resources :pages
-  resources :ctlinks 
+  resources :pages do
+    collection { post :import }
+    collection { get :api }
+  end
+  resources :ctlinks
   resources :domains do
     resources :nodes
-  end  
+  end
   resources :nodes do
     resources :posts
   end
   resources :posts do
-    collection {post :import}
-    collection {post :youtube_import}
-    collection {post :ptt_import}
-    collection {get :api}
-    collection {get :dashboard}
-    collection {get :search}
-    collection {get :news}
+    collection { post :import }
+    collection { post :youtube_import }
+    collection { post :ptt_import }
+    collection { get :api }
+    collection { get :dashboard }
+    collection { get :search }
+    collection { get :news }
   end
   resources :links
-  root to: "posts#index"
+  root to: 'posts#index'
 end
