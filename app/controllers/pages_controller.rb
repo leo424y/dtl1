8a899aede0e8939eec8c590d8751a42ba3ea13ef
Fly Chang <leo424y@gmail.com>
2020-04-20 18:13:28 +0800

    map daily domain result

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,18 @@ class PagesController < ApplicationController
   include Response
   skip_before_action :verify_authenticity_token
 
+  def index
+    @page = Page.count_daily_domain 
+    domains = []
+    @page.each do |p|
+      domains << get_host_without_www(p.link)
+    end
+    grouped_domains = domains.group_by(&:capitalize).map {|k,v| [k, v.length]}.sort_by { |w| w[1] }.reverse
+
+    json_response({result: grouped_domains})
+  end
+
+
   def create
     @page = Page.new(page_params)
     if @page.save
@@ -18,4 +30,10 @@ private
 
 def page_params
   params.require(:pages).permit(:uname, :pid, :ptitle, :ptype, :pdescription, :ptime, :mtime, :url, :link, :platform, :score)
+end
+
+def get_host_without_www(url)
+  url = "http://#{url}" if URI.parse(url).scheme.nil?
+  host = URI.parse(url).host.downcase
+  host.start_with?('www.') ? host[4..-1] : host
 end
